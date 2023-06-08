bc4e0d5264116073f7a480c5f696522e935d737e
Ose-Augustine <osebeckley@gmail.com>
2023-06-08 23:58:28 +0100

    Created scene and camera

---
+++ src/world/components/camera.js
@@ -0,0 +1,11 @@
+import { PerspectiveCamera } from "three";
+
+function createCamera() {
+    let camera = new PerspectiveCamera(45, 1, 0.1, 1000); 
+    // Set camera far from cube
+    camera.position.z =  10; 
+
+    return camera; 
+}
+
+export { createCamera }
