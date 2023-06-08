bd3b96fa14a6bb5d6b2d5a9ecce677d603f8911c
Ose-Augustine <osebeckley@gmail.com>
2023-06-09 00:10:14 +0100

    Create meshes

---
+++ src/world/components/meshes.js
@@ -0,0 +1,12 @@
+import { MeshStandardMaterial, Mesh, BoxGeometry} from "three";
+
+function createMesh() {
+    const geometry = new BoxGeometry(10,10,10); 
+    const material = new MeshStandardMaterial({color:'red'}); 
+
+    const mesh = new Mesh(geometry, material); 
+
+    return mesh; 
+}
+
+export { createMesh }; 
