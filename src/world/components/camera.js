af29b5bc6fcac7bacb6f9eb90b3f1f800b78ed54
Ose-Augustine <osebeckley@gmail.com>
2023-06-09 01:31:16 +0100

    Readjust camera

--- src/world/components/camera.js
+++ src/world/components/camera.js
@@ -3,7 +3,7 @@ import { PerspectiveCamera } from "three";
 function createCamera() {
     let camera = new PerspectiveCamera(45, 1, 0.1, 1000); 
     // Set camera far from cube
-    camera.position.z =  10; 
+    camera.position.z =  60; 
 
     return camera; 
 }
