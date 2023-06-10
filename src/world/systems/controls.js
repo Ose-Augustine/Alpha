3a176fbe41e72a7cc806430a060d6522bfd41488
Ose-Augustine <osebeckley@gmail.com>
2023-06-10 14:24:43 +0100

    Rename fbx file

--- src/world/systems/controls.js
+++ src/world/systems/controls.js
@@ -1,4 +1,5 @@
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { Vector3 } from 'three';
 
 function createControls(camera, canvas) {
     const controls = new OrbitControls(camera, canvas); 
